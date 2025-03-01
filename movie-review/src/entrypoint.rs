cdabc13f2e69e7c73df415a9eef3b3fc7f76af07
ennea8 <ennea8@gmail.com>
2025-03-01 23:31:51 +0800

    refactor

---
+++ movie-review/src/entrypoint.rs
@@ -0,0 +1,33 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke_signed,
+    pubkey::Pubkey,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
+};
+
+use crate::processor;
+
+entrypoint!(process_instruction);
+
+// Inside lib.rs
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!(
+        "process_instruction: {}: {} accounts, data={:?}",
+        program_id,
+        accounts.len(),
+        instruction_data
+    );
+    processor::process_instruction(program_id, accounts, instruction_data)?;
+
+    Ok(())
+}
